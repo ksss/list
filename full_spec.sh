3e54e61188189335d66c8746f812bdb9bf34cd99
ksss <co000ri@gmail.com>
2014-01-25 00:28:34 +0900

    full spec script

---
+++ full_spec.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# this script need installed rbenv and installed all ruby version
+
+VERSIONS=(1.9.3-p484 2.0.0-p247 2.1.0)
+ORIG=`rbenv version-name`
+
+for version in ${VERSIONS[@]}
+do
+  rbenv global ${version}
+  rbenv rehash
+  bundle exec rake
+done
+rbenv global ${ORIG} && rbenv rehash
+
