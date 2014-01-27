d9050155cfc1898053128a0a636061207f79efdd
ksss <co000ri@gmail.com>
2014-01-27 22:36:44 +0900

    fix comment

--- full_spec.sh
+++ full_spec.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# this script need installed rbenv and installed all ruby version
+# this script need installed *rbenv* and installed all ruby version
 
 VERSIONS=(1.9.3-p484 2.0.0-p247 2.1.0)
 ORIG=`rbenv version-name`
